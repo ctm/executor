dcab9a0605718d35a7a1c4d84870bb6177817d6c
Wolfgang Thaller <wolfgang.thaller@gmx.net>
2017-11-12 04:22:05 +0100

    fix line endings on source that might never be compiled again

--- src/config/front-ends/nextstep/HFS_XFer/HFS_XFer_DA/mytype.h
+++ src/config/front-ends/nextstep/HFS_XFer/HFS_XFer_DA/mytype.h
@@ -1,1 +1,22 @@
-#if !defined(UNIX)typedef char BOOLEAN;typedef short INTEGER;typedef long LONGINT;#define LONGORPTR Ptrtypedef unsigned short ushort;#else#define LONGORPTR LONGINT#include		<rsys/libcproto.h>#endiftypedef unsigned long  ulong;#if		!defined(UNIX)#define PRIVATE static#define PUBLIC#endif	/* UNIX */
+#if !defined(UNIX)
+
+typedef char BOOLEAN;
+typedef short INTEGER;
+typedef long LONGINT;
+#define LONGORPTR Ptr
+
+typedef unsigned short ushort;
+
+#else
+
+#define LONGORPTR LONGINT
+#include		<rsys/libcproto.h>
+
+#endif
+
+typedef unsigned long  ulong;
+
+#if		!defined(UNIX)
+#define PRIVATE static
+#define PUBLIC
+#endif	/* UNIX */
