ae9f38d9c7eafce8ec038e60d12710fa5793dc86
Wolfgang Thaller <wolfgang.thaller@gmx.net>
2017-11-16 21:58:51 +0100

    Revert "kill icon_item_template_t in favour of direct pointer work (for now at least)"
    This reverts commit f5081f9b0add6d82464ae1bc6c128369f6657ee3.
    
    This was just a workaround that's no longer needed.
    
    # Conflicts:
    #	src/dialAlert.cpp
    #	src/include/rsys/dial.h

--- src/include/rsys/dial.h
+++ src/include/rsys/dial.h
@@ -2,5 +2,15 @@
 #define __rsys_dial_h__
 namespace Executor
 {
+struct icon_item_template_t
+{
+    GUEST_STRUCT;
+    GUEST<int16> count;
+    GUEST<Handle> h;
+    GUEST<Rect> r;
+    GUEST<uint8> type;
+    GUEST<uint8> len;
+    GUEST<int16> res_id;
+};
 }
 #endif /* !defined (__rsys_dial_h__) */
