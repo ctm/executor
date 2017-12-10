b668fc74f00666ffc1ae074f324d1004f8583081
Wolfgang Thaller <wolfgang.thaller@gmx.net>
2017-12-10 15:28:25 +0100

    remove unused stuff from version.h

--- src/include/rsys/version.h
+++ src/include/rsys/version.h
@@ -28,45 +28,6 @@ extern "C" {
 extern const char ROMlib_executor_version[];
 extern const char *ROMlib_executor_full_name;
 
-#define PLATFORM_MOD 10 /* last decimal digit of serial number is platform */
-#define COMPOSITE_MOD 100 /* 2nd to last digit is price class */
-
-#if defined(NEXTSTEP)
-#define VERSION_SIG PLATFORM_NEXTSTEP
-#elif defined(MSDOS)
-#define VERSION_SIG PLATFORM_DOS
-#elif defined(CYGWIN32)
-#define VERSION_SIG PLATFORM_CYGWIN32
-#elif defined(LINUX)
-#define VERSION_SIG PLATFORM_LINUX
-#elif defined(MACOSX_) || defined(MACOSX)
-#define VERSION_SIG PLATFORM_MACOSX
-#else
-#error "Need VERSION_SIG #define"
-#endif
-
-enum
-{
-    CLASS_COMMERCIAL = 0,
-    CLASS_EDUCATIONAL = 10,
-    CLASS_STUDENT = 20
-};
-enum
-{
-    PLATFORM_NEXTSTEP = 0,
-    PLATFORM_CYGWIN32 = 1,
-    PLATFORM_DOS = 2,
-    PLATFORM_LINUX = 3,
-    PLATFORM_ANY = 4,
-    PLATFORM_MACOSX = 5
-};
-
-enum
-{
-    STUDENT_DOS = CLASS_STUDENT + PLATFORM_DOS,
-    STUDENT_LINUX = CLASS_STUDENT + PLATFORM_LINUX,
-};
-
 #define EXECUTOR_NAME "executor"
 
 extern void ROMlib_set_system_version(uint32 version);
