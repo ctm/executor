c4623cb0a3c7f28fb98b8b7e34ae1f11b3356dc8
Wolfgang Thaller <wolfgang.thaller@gmx.net>
2017-12-17 23:05:14 +0100

    byteswap implementation for MSVC

--- src/config/arch/x86_64/x86_64.h
+++ src/config/arch/x86_64/x86_64.h
@@ -7,8 +7,15 @@
 /* TODO: only do these if the compiler supports it, check to see if we can
          get a better swap16 w/o using builtin_bswap32 */
 
+#ifdef _MSC_VER
+#include <stdlib.h>
+
+#define swap16(v) _byteswap_ushort(v)
+#define swap32(v) _byteswap_ulong(v)
+#else
 #define swap16(v) ((uint16_t)(__builtin_bswap32((int32_t)(v)) >> 16))
 
 #define swap32(v) ((uint32_t)__builtin_bswap32((int32_t)(v)))
+#endif
 
 #endif /* !defined (__arch_x86_64_h__) */
